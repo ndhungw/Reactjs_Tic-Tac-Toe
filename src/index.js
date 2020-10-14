cb685c0f94fdf8bf7a1348b8b59b4e4d46fb2e57
ndhungw <ndh1379@gmail.com>
2020-10-14 18:42:16 +0700

    convert to 2d array completed

--- src/index.js
+++ src/index.js
@@ -11,43 +11,45 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board({ squares, onClick }) {
-  function renderSquare(i) {
-    return (
-      <Square
-        value={squares[i]}
-        onSquareClick={() => onClick(i)}
-      />
-    );
+  function renderSquare(i, j) {
+    return <Square value={squares[i][j]} onSquareClick={() => onClick(i, j)} />;
   }
 
-    return (
-      <div>
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
-      </div>
-    );
+  const board = squares.map((row, i) => (
+    <div className="board-row">
+      {row.map((col, j) => {
+        return renderSquare(i, j);
+      })}
+    </div>
+  ));
+
+  return <div>{board}</div>;
 }
 
+// function HistoryButton({move, description, isClicked}) {
+//   return (
+//     <button
+//     onClick={() => this.jumpTo(move)}
+//     isClicked={isClicked}
+//     >{description}</button>
+//   );
+// }
+
+const maxRow = 3;
+const maxCol = 3;
 class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       history: [
-        { 
-          squares: Array(9).fill(null),
+        {
+          squares: Array(maxRow)
+            .fill()
+            .map(() => Array(maxCol).fill(null)),
+          moveDescription: {
+            row: null,
+            col: null,
+          },
         },
       ],
       stepNumber: 0,
@@ -55,74 +57,85 @@ class Game extends React.Component {
     };
   }
 
-  handleClick(i) {
+  handleClick(i, j) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
-    const squares = current.squares.slice();
+    // deep copy
+    const squares = current.squares.map((row) => {
+      return row.slice();
+    });
 
-    if (calculateWinner(squares) || squares[i]) {
+    if (calWinner(squares) || squares[i][j]) {
       return;
     }
 
-    squares[i] = this.state.xIsNext ? "X" : "O";
+    squares[i][j] = this.state.xIsNext ? "X" : "O";
+
+    const moveDescription = {
+      row: i,
+      col: j,
+    };
 
     this.setState({
       history: [
         ...history,
-        {squares}
+        {
+          squares,
+          moveDescription,
+        },
       ],
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
-
-    // this.setState({
-    //   history: history.concat([{
-    //     squares: squares
-    //   }]),
-    //   xIsNext: !this.state.xIsNext,
-    // });
   }
 
   jumpTo(step) {
+    this.isClicked = true;
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) === 0
-    })
+      xIsNext: step % 2 === 0,
+    });
   }
-  
 
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = calWinner(current.squares);
     let status;
 
     if (winner) {
       status = "Winner: " + winner;
-    } else if (current.squares.every((square) => square !== null)) {
+    } else if (this.state.stepNumber === maxRow * maxCol) {
       status = "Draw!";
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
 
     const moves = history.map((step, move) => {
-      const description = move !== 0 ? 'Go to the move #' + move : 'Go to the start';
+      const description =
+        move !== 0
+          ? "Go to the move #" +
+            move +
+            " (row " +
+            step.moveDescription.row +
+            " col " +
+            step.moveDescription.col +
+            ")"
+          : "Go to the start";
 
       return (
         <li key={move}>
-          <button
-          onClick={() => this.jumpTo(move)}
-          >{description}</button>
+          <button onClick={() => this.jumpTo(move)}>{description}</button>
         </li>
-      )
-    })
+      );
+    });
 
     return (
       <div className="game">
         <div className="game-board">
-          <Board 
-          squares={current.squares}
-          onClick={(i) => this.handleClick(i)}
+          <Board
+            squares={current.squares}
+            onClick={(i, j) => this.handleClick(i, j)}
           />
         </div>
         <div className="game-info">
@@ -138,23 +151,70 @@ class Game extends React.Component {
 
 ReactDOM.render(<Game />, document.getElementById("root"));
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+function checkDiagonal(squares) {
+  let result = "";
+  for (let idx = 0; idx < squares.length; idx++) {
+    if (squares[idx][idx]) {
+      result += squares[idx][idx];
     }
   }
+  return result === "XXX" ? "X" : result === "OOO" ? "O" : null;
+}
 
+function checkAntiDiagonal(squares) {
+  let result = "";
+
+  for (let colIdx = 0; colIdx < squares.length; colIdx++) {
+    let rowIdx = squares.length - 1 - colIdx;
+
+    if (squares[rowIdx][colIdx]) {
+      result += squares[rowIdx][colIdx];
+    }
+  }
+  return result === "XXX" ? "X" : result === "OOO" ? "O" : null;
+}
+
+function checkRows(squares) {
+  for (let rowIdx = 0; rowIdx < squares.length; rowIdx++) {
+    let result = "";
+    for (let colIdx = 0; colIdx < squares[0].length; colIdx++) {
+      if (squares[rowIdx][colIdx]) {
+        result += squares[rowIdx][colIdx];
+      }
+    }
+
+    if (result === "XXX") {
+      return "X";
+    } else if (result === "OOO") {
+      return "O";
+    }
+  }
   return null;
+}
+
+function checkCols(squares) {
+  for (let colIdx = 0; colIdx < squares[0].length; colIdx++) {
+    let result = "";
+    for (let rowIdx = 0; rowIdx < squares.length; rowIdx++) {
+      if (squares[rowIdx][colIdx]) {
+        result += squares[rowIdx][colIdx];
+      }
+    }
+
+    if (result === "XXX") {
+      return "X";
+    } else if (result === "OOO") {
+      return "O";
+    }
+  }
+  return null;
+}
+
+function calWinner(squares) {
+  return (
+    checkDiagonal(squares) ||
+    checkAntiDiagonal(squares) ||
+    checkRows(squares) ||
+    checkCols(squares)
+  );
 }
